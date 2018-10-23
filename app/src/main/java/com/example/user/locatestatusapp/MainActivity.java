2f29655a152b2f78071a9eb292b2440c4b7b326e
UnKiGwon <qlrmr111@naver.com>
2018-10-23 19:40:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/locatestatusapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.locatestatusapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+
+    }
+
+}
